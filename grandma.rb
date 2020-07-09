26aa68b0b08f9cd16c1c3e9e76171d4abe40117e
Brittney Galvan <brittney.galvan25@yahoo.com>
2020-07-09 01:35:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,13 @@
-speak_to_grandma(phrase).upcase 
-if phrase != "Hi Grandma!".upcase
-  puts "HUH?! SPEAK UP, SONNY!".upcase
-elsif phrase == "Hi Grandma!".upcase
-  puts "NO, NOT SINCE 1938!".upcase
-else phrase == "I LOVE YOU GRANDMA!".upcase 
-  puts "I LOVE YOU TOO PUMPKIN!".upcase
-end 
+def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  end
+  if phrase != phrase.upcase()
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
   
 
 
